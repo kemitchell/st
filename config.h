60a534a3fc9407600f89a4e51ea281b9a1534f26
Kyle E. Mitchell <kyle@kemitchell.com>
2020-05-17 19:59:33 -0700

    Customize default colors

--- config.h
+++ config.h
@@ -115,10 +115,10 @@ static const char *colorname[] = {
  * Default colors (colorname index)
  * foreground, background, cursor, reverse cursor
  */
-unsigned int defaultfg = 7;
+unsigned int defaultfg = 15;
 unsigned int defaultbg = 0;
-static unsigned int defaultcs = 256;
-static unsigned int defaultrcs = 257;
+static unsigned int defaultcs = 7;
+static unsigned int defaultrcs = 15;
 
 /*
  * Default shape of cursor
